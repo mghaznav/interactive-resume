508ab2d59ef902f7deb1bfe842d8ffea1d901faf
Ali Ghaznavi <alighaznavi10@gmail.com>
2024-07-30 12:51:13 -0400

    Improved the styling of the cards and made them more responsive to screen size

--- src/components/ScrollSection.jsx
+++ src/components/ScrollSection.jsx
@@ -1,8 +1,31 @@
-import { useRef } from 'react';
+import { useRef, useEffect, useState } from 'react';
 import { motion, useScroll, useTransform } from 'framer-motion';
 
 export default function ScrollSection() {
+  const getWidth = (width) => {
+    if (width < 640) {
+      return 2;
+    } else if (width < 768) {
+      return 3;
+    } else {
+      return 4;
+    }
+  };
+
+  const [height, setHeight] = useState(getWidth(window.innerWidth));
+
+  useEffect(() => {
+    const handleResize = () => {
+      setHeight(getWidth(window.innerWidth));
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  // Scroll Animations
   const contentCardRef = useRef(null);
+
   const { scrollYProgress } = useScroll({
     target: contentCardRef,
     offset: ["start end", "end start"]
@@ -18,32 +41,31 @@ export default function ScrollSection() {
     scrollYProgress,
     [0.2, 0.5, 0.8],
     [
-      '0px 0px 00px rgba(0, 0, 0, 0.2)',
-      '0px 0px 20px rgba(0, 0, 0, 0.5)',
-      '0px 0px 00px rgba(0, 0, 0, 0.2)'
+      '20px 20px 60px #bebebe, -20px -20px 60px #ffffff',
+      '20px 20px 60px #bebebe, -20px -20px 60px #bebebe',
+      '20px 20px 60px #bebebe, -20px -20px 60px #ffffff'
     ]
   );
 
   return (
-    <section>
-      <div className="h-[150vh] bg-blue-200 p-8 relative">
-        <motion.article
-          ref={contentCardRef}
-          style={{
-            height: `calc(100vh - ${64}px)`,
-            scale,
-            boxShadow
-          }}
-          className="h-[100vh] bg-white sticky top-8 z-0 rounded-3xl p-3"
-        >
-          <p>This is where the content will be</p>
-        </motion.article>
-      </div>
-      {/* <div className="h-96 bg-white">
-        <article>
-          <h1>Additional content will go here</h1>
-        </article>
-      </div> */}
-    </section>
+    <motion.section
+      style={{
+      padding: `${height}rem`
+    }}
+    className="h-[150vh] bg-[#e0e0e0] relative"
+    >
+      <motion.article
+        ref={contentCardRef}
+        style={{
+          height: `calc(100vh - ${height * 2}rem)`,
+          top: `${height}rem`,
+          scale,
+          boxShadow
+        }}
+        className="h-[100vh] bg-white sticky z-0 rounded-3xl p-3"
+      >
+        <p>This is where the content will be</p>
+      </motion.article>
+    </motion.section>
   );
 }
