35446b2b7ac1c112174971d3c64942de71ad635b
Ali Ghaznavi <alighaznavi10@gmail.com>
2024-07-31 20:14:20 -0400

    Made Hero more responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,13 +4,14 @@ export default function Hero() {
   return (
     <header className="flex h-[100vh] relative">
         <div className="bg-white lg:w-2/3 w-full flex flex-col items-center justify-center">
-          <div className="w-2/3">
-            <h2 className="text-6xl font-bold my-8 [text-shadow:_-2px_5px_10px_rgb(0_0_0_/_20%)]">
+          <div className="md:w-2/3 w-3/4">
+            <h2 className="md:text-6xl md:my-8 text-4xl my-4 font-bold [text-shadow:_-2px_5px_10px_rgb(0_0_0_/_20%)]">
               Hello, My name&apos;s <span className="text-navy-blue">Muhammad Ali</span> Ghaznavi
             </h2>
-            <h3 className='text-4xl my-8 text-navy-blue [text-shadow:_-2px_5px_10px_rgb(0_0_0_/_20%)]'>I&apos;m a Software Developer</h3>
-            <button className="bg-navy-blue hover:bg-white text-white hover:text-navy-blue font-bold p-4 rounded-lg
-                              [box-shadow:_-2px_10px_20px_rgb(0_0_0_/_20%)] hover:[box-shadow:_-2px_10px_20px_rgb(0_0_0_/_40%)]">
+            <h3 className='md:text-4xl md:my-8 text-3xl my-4 text-navy-blue [text-shadow:_-2px_5px_10px_rgb(0_0_0_/_20%)]'>I&apos;m a Software Developer</h3>
+            <button
+              className="bg-navy-blue hover:bg-white text-white hover:text-navy-blue font-bold
+                        md:p-4 md:text-base p-3 text-sm my-4 rounded-lg [box-shadow:_-2px_10px_20px_rgb(0_0_0_/_20%)]">
               Download Resume
             </button>
           </div>
