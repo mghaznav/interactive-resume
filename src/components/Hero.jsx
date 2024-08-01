85bce6646b9428e87151b7f4fd25ab685f3f57d1
Ali Ghaznavi <alighaznavi10@gmail.com>
2024-08-01 17:34:49 -0400

    Fixed some HTML semantic issues

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,22 +2,22 @@ import headshot from '../assets/headshot.png';
 
 export default function Hero() {
   return (
-    <header className="flex h-[100vh] relative">
-        <div className="bg-white lg:w-2/3 w-full flex flex-col items-center justify-center">
-          <div className="md:w-2/3 w-3/4">
-            <h2 className="md:text-6xl md:my-8 text-4xl my-4 font-bold [text-shadow:_-2px_5px_10px_rgb(0_0_0_/_20%)]">
-              Hello, My name&apos;s <span className="text-navy-blue">Muhammad Ali</span> Ghaznavi
-            </h2>
-            <h3 className='md:text-4xl md:my-8 text-3xl my-4 text-navy-blue [text-shadow:_-2px_5px_10px_rgb(0_0_0_/_20%)]'>I&apos;m a Software Developer</h3>
-            <button
-              className="bg-navy-blue hover:bg-white text-white hover:text-navy-blue font-bold transition-all duration-300
-                        md:p-4 md:text-base p-3 text-sm my-4 rounded-lg [box-shadow:_-2px_10px_20px_rgb(0_0_0_/_20%)]">
-              Download Resume
-            </button>
-          </div>
+    <section className="flex h-[100vh] relative">
+      <div className="bg-white lg:w-2/3 w-full flex flex-col items-center justify-center">
+        <div className="md:w-2/3 w-3/4">
+          <h2 className="md:text-6xl md:my-8 text-4xl my-4 font-bold [text-shadow:_-2px_5px_10px_rgb(0_0_0_/_20%)]">
+            Hello, My name&apos;s <span className="text-navy-blue">Muhammad Ali</span> Ghaznavi
+          </h2>
+          <h3 className='md:text-4xl md:my-8 text-3xl my-4 text-navy-blue [text-shadow:_-2px_5px_10px_rgb(0_0_0_/_20%)]'>I&apos;m a Software Developer</h3>
+          <button
+            className="bg-navy-blue hover:bg-white text-white hover:text-navy-blue font-bold transition-all duration-300
+                      md:p-4 md:text-base p-3 text-sm my-4 rounded-lg [box-shadow:_-2px_10px_20px_rgb(0_0_0_/_20%)]">
+            Download Resume
+          </button>
         </div>
-        <div className="bg-navy-blue w-1/3 hidden lg:block"></div>
-        <img src={headshot} className="absolute bottom-0 left-2/3 h-[75%] -translate-x-1/2 hidden lg:block" />
-    </header>
+      </div>
+      <div className="bg-navy-blue w-1/3 hidden lg:block"></div>
+      <img src={headshot} className="absolute bottom-0 left-2/3 h-[75%] -translate-x-1/2 hidden lg:block" />
+    </section>
   );
 }
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -8,22 +8,22 @@ export default function Navigation() {
   const socialLinksClasses = "text-6xl text-white/75 hover:text-white";
 
   return (
-    <nav className="flex fixed top-0 h-24 w-full z-10 items-center">
+    <header className="flex fixed top-0 h-24 w-full z-10 items-center">
       <div className="w-2/3 px-8 flex justify-between items-center">
         <img src={logo} className="h-16"/>
-        <div className="flex gap-8 w-2/3 justify-center content-justify">
+        <nav className="flex gap-8 w-2/3 justify-center content-justify">
           <a href="#" className={pageLinksClasses}>About Me</a>
           <a href="#" className={pageLinksClasses}>Work Experiences</a>
           <a href="#" className={pageLinksClasses}>Projects</a>
           <a href="#" className={pageLinksClasses}>Blog</a>
           <a href="#" className={pageLinksClasses}>Contact</a>
-        </div>
+        </nav>
       </div>
       <div className="w-1/3 px-8 flex items-center justify-end gap-8">
         <a href="#" className={socialLinksClasses}><FaSquareGithub /></a>
         <a href="#" className={socialLinksClasses}><FaLinkedin /></a>
         <a href="#" className={socialLinksClasses}><FaMedium /></a>
       </div>
-    </nav>
+    </header>
   );
 }
